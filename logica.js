bc2aecd804610e61d107b27de17768851b9189ec
Igunho <87882050+Igunho@users.noreply.github.com>
2025-06-04 01:55:03 -0300

    Fix + Feature
    Corrigi os erros nos valores
    Quantidade agora é alterada ao clicar no botão outra vez

--- logica.js
+++ logica.js
@@ -3,36 +3,48 @@ const itens = document.getElementById("lista_itens")
 function atualizarTotal() {
     let qtJogos = document.querySelectorAll(".jogo")
 
+    let total  = 0
     for(let i = 0; i < qtJogos.length; i++) {
         const ExibirTotal = document.getElementById("valorTotal")
 
         qtJogos[i].lastChild.textContent = qtJogos[i].children[1].textContent * qtJogos[i].children[2].firstChild.value
 
-        let total  = 0
-        total = total + parseFloat(qtJogos[i].children[1].textContent)
+        total = total + parseFloat(qtJogos[i].lastChild.textContent)
 
-        console.log(qtJogos[i].lastChild.textContent) //Total
-        console.log(qtJogos[i].children[1].textContent) //Valor do jogo
-        console.log(qtJogos[i].children[2].firstChild.value)  //Quantidade especificada
-
-        ExibirTotal.textContent = "Valor: " + total
+        ExibirTotal.textContent = "Valor: " + total.toFixed(2)
     }
 
-    //setTimeout(atualizarTotal,1000)
+    setTimeout(atualizarTotal,1000)
 }
 
 function adicionar(item) {
     if(itens.innerHTML.includes(item.name)) {
-        console.log("jogo já incluido")
+	document.getElementById(item.parentElement.id).value =  parseInt(document.getElementById(item.parentElement.id).value) + 1
+	console.log(parseInt(document.getElementById(item.parentElement.id).value))
         atualizarTotal()
-    } else { itens.innerHTML +=  "<tr class='jogo'>" +
-                                "<td>" + item.name + "</td>" +
-                                "<td>" + item.value + "</td>" +
-                                "<td><input type='text' value='1' id="+item.parentElement.id+"></input></td>" +
-                                "<td>"+ item.value +"</td>" +
-                            "</tr>"
-    }
+    } else { 
+	    const tablerow = document.createElement("tr");
+	    tablerow.className = "jogo";
+	    const nameTD = document.createElement("td");
+	    const nameText = document.createTextNode(item.name);
+	    nameTD.appendChild(nameText);
+	    const valueTD = document.createElement("td");
+	    const valueText = document.createTextNode(item.value);
+	    valueTD.appendChild(valueText);
+	    const inputTD = document.createElement("td");
+	    const input =  document.createElement("input");
+	    input.type = "text";
+	    input.value = "1";
+	    input.id = item.parentElement.id
+	    inputTD.appendChild(input);
+	    const totalTD = document.createElement("td");
+	    const total = document.createTextNode(item.value);
+	    totalTD.appendChild(total);
 
-    //console.log(document.getElementById(item.parentElement.id).parentElement.parentElement.lastChild)
-    //console.log(itens)
+	    tablerow.appendChild(nameTD);
+	    tablerow.appendChild(valueTD);
+	    tablerow.appendChild(inputTD);
+	    tablerow.appendChild(totalTD);
+	    itens.appendChild(tablerow);
+    }
 }
